de97bf0cf7b42d72fe9464ca36c06e8f79a4a026
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 22:00:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key")
+    SECRET_KEY = "your-secret-key"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///local.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
 
-    # SENDGRID SMTP
+    # 🔹 SendGrid / Mail settings
     MAIL_SERVER = "smtp.sendgrid.net"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -14,4 +13,4 @@ class Config:
     MAIL_USERNAME = "apikey"
     MAIL_PASSWORD = os.environ.get("SENDGRID_API_KEY")
 
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_DEFAULT_SENDER = "noreply.security.training@gmail.com"
