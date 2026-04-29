21fa5be77dbdf079ad5fd5dc78cb7898516646f7
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 18:38:43 +0300

    Add User and Click models with SQLAlchemy

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    risk_score = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Click(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    ip = db.Column(db.String(50))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
