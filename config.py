e4b93f494c3e1d29b05ade7ff715ffe27843dfc0
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 20:54:16 +0300

    Update email configuration to use SendGrid

--- config.py
+++ config.py
@@ -3,19 +3,15 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key")
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "sqlite:///local.db"
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MAIL
-    MAIL_SERVER = "smtp.gmail.com"
+    # SENDGRID SMTP
+    MAIL_SERVER = "smtp.sendgrid.net"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
+
+    MAIL_USERNAME = "apikey"
+    MAIL_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
