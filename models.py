98635c48304cca668a782b27fdb19ba7e80ea65f
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 19:29:06 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 db = SQLAlchemy()
-
+risk_score = db.Column(db.Integer, default=0)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
