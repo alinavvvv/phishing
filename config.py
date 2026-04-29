a0f244bf68d28ef1016c6e1b4709d932e92d6c35
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 20:51:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
