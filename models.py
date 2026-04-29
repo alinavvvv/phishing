5d7ceae3f3864fef169b1524a85f4c85c1749749
alinavvvv <volkanovaodessa@gmail.com>
2026-04-29 19:56:07 +0300

    Add ForeignKey constraint to Click model
    Added ForeignKey constraint to user_id in Click model.

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
-risk_score = db.Column(db.Integer, default=0)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     risk_score = db.Column(db.Integer, default=0)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Click(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    ip = db.Column(db.String(50))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
