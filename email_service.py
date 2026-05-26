7f2c9021e88a205f264e94b2e78392a73d48474d
alinavvvv <volkanovaodessa@gmail.com>
2026-05-26 21:55:14 +0300

    Refactor email service to send training emails
    Refactored email sending function to use Flask template for HTML content and added support for optional parameters.

--- email_service.py
+++ email_service.py
@@ -1,24 +1,34 @@
 import os
+from flask import render_template
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def send_phishing_email(user, link):
+
+def send_training_email(user, link, campaign=None, open_pixel_url=None):
+    html_content = render_template(
+        "emails/security_alert.html",
+        user=user,
+        link=link,
+        campaign=campaign,
+        open_pixel_url=open_pixel_url
+    )
+
     message = Mail(
-        from_email="noreply.security.training@gmail.com",
+        from_email=os.environ.get(
+            "SENDGRID_FROM_EMAIL",
+            "noreply.security.training@gmail.com"
+        ),
         to_emails=user.email,
-        subject="Security Training Alert",
-        html_content=f"""
-        <h3>Security Awareness Training</h3>
-        <p>Click the link to verify login attempt:</p>
-        <a href="{link}">Verify</a>
-        """
+        subject="Обучение по киберсигурност",
+        html_content=html_content
     )
 
     try:
         sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
         response = sg.send(message)
-        print("STATUS:", response.status_code)
+        print("SENDGRID STATUS:", response.status_code)
+        return True
 
     except Exception as e:
         print("SENDGRID ERROR:", e)
-        raise e
+        return False
