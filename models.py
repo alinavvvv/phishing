1d369e8914e2a2c762d42e9b7b16f00b288b2085
alinavvvv <volkanovaodessa@gmail.com>
2026-06-08 16:07:08 +0300

    Refactor models to include table names and relationships
    Added __tablename__ attributes and back_populates relationships to models.

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
 
     first_name = db.Column(db.String(100))
@@ -20,18 +21,32 @@ class User(db.Model):
 
     risk_score = db.Column(db.Integer, default=0)
 
-    clicks = db.relationship("Click", backref="user", lazy=True)
+    # Връзки, сочещи към другите модели чрез back_populates
+    clicks = db.relationship("Click", back_populates="user", lazy=True)
+    email_events = db.relationship("EmailEvent", back_populates="user", lazy=True)
+    training_progress = db.relationship("TrainingProgress", back_populates="user", lazy=True)
+
+    def click_rate(self):
+        sent_count = EmailEvent.query.filter_by(user_id=self.id).count()
+        if sent_count == 0:
+            return 0
+        clicked_count = EmailEvent.query.filter_by(user_id=self.id, clicked=True).count()
+        return round((clicked_count / sent_count) * 100, 1)
 
 
 class Campaign(db.Model):
+    __tablename__ = 'campaign'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     template_name = db.Column(db.String(120), default="security_alert.html")
     frequency = db.Column(db.String(50), default="manual")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    email_events = db.relationship("EmailEvent", back_populates="campaign", lazy=True)
+
 
 class EmailEvent(db.Model):
+    __tablename__ = 'email_event'
     id = db.Column(db.Integer, primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -50,11 +65,15 @@ class EmailEvent(db.Model):
     clicked_at = db.Column(db.DateTime)
     click_count = db.Column(db.Integer, default=0)
 
-    user = db.relationship("User", backref="email_events")
-    campaign = db.relationship("Campaign", backref="email_events")
+    # Връзки
+    user = db.relationship("User", back_populates="email_events")
+    campaign = db.relationship("Campaign", back_populates="email_events")
+    clicks = db.relationship("Click", back_populates="email_event", lazy=True)
+    training_progress = db.relationship("TrainingProgress", back_populates="email_event", lazy=True)
 
 
 class Click(db.Model):
+    __tablename__ = 'click'
     id = db.Column(db.Integer, primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -64,11 +83,13 @@ class Click(db.Model):
     user_agent = db.Column(db.String(300))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-    user = db.relationship("User", backref="clicks")
-    email_event = db.relationship("EmailEvent", backref="clicks")
+    # Връзки
+    user = db.relationship("User", back_populates="clicks")
+    email_event = db.relationship("EmailEvent", back_populates="clicks")
 
 
 class TrainingProgress(db.Model):
+    __tablename__ = 'training_progress'
     id = db.Column(db.Integer, primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -77,5 +98,6 @@ class TrainingProgress(db.Model):
     completed = db.Column(db.Boolean, default=False)
     completed_at = db.Column(db.DateTime)
 
-    user = db.relationship("User", backref="training_progress")
-    email_event = db.relationship("EmailEvent", backref="training_progress")
+    # Връзки
+    user = db.relationship("User", back_populates="training_progress")
+    email_event = db.relationship("EmailEvent", back_populates="training_progress")
